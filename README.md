b0654f9b8fbe97fcfbc2fcb5e3cef61a7c4aa1b2
Hillary Mwashi <mwashihillary@gmail.com>
2025-01-24 15:27:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Elimika__FRONTEND
+ The learning content management solution for Kenya higher learning institutions
---
+++ android/app/src/main/kotlin/com/swizzy_industries/elimika_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.swizzy_industries.elimika_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
